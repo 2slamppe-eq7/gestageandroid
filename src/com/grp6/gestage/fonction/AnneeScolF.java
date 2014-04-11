28e5166b4b1e2bdd1d99d12f6efb9f755ffaea95
Amaury LECLERC <amauryleclerc@hotmail.fr>
2014-04-11 16:47:05 +0200

    test

---
+++ src/com/grp6/gestage/fonction/AnneeScolF.java
@@ -0,0 +1,89 @@
+package com.grp6.gestage.fonction;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+
+import com.grp6.gestage.library.DatabaseHandler;
+import com.grp6.gestage.library.JSONParser;
+import com.grp6.gestage.metier.AnneeScol;
+import com.grp6.gestage.metier.Personne;
+
+public class AnneeScolF  extends Config {
+
+	private JSONParser jsonParser;
+	
+	
+
+	// constructor
+	public AnneeScolF( ){
+		jsonParser = new JSONParser( );
+	}
+	
+	
+	/**
+	 * Function get Login status
+	 * */
+	public boolean isUserLoggedIn(Context context){
+		DatabaseHandler db = new DatabaseHandler(context);
+		int count = db.getRowCount();
+		if(count > 0){
+			// user logged in
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Function to logout user
+	 * Reset Database
+	 * */
+	public boolean logoutUser(Context context){
+		DatabaseHandler db = new DatabaseHandler(context);
+		db.resetTables();
+		return true;
+	}
+	
+	public List<AnneeScol> getAll() throws JSONException, IllegalStateException, IOException{
+		ArrayList<AnneeScol> lesAnneeScols = new ArrayList<AnneeScol>();
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", "annee"));
+		params.add(new BasicNameValuePair("fonc", "getAll"));
+		JSONObject json = jsonParser.getJSONFromUrl(URL, params);
+		JSONArray json_annees = json.getJSONArray("annees");
+		for (int i = 0; i < json_annees.length(); i++) {
+			//	JSONObject catObj = (JSONObject) json_chantier.get(i);
+			lesAnneeScols.add(chargerUnEnregistrement((JSONObject) json_annees.get(i)));
+			//	lesChantiers.add(cat);
+			}
+	
+	
+		return lesAnneeScols;
+	}
+
+	
+	private AnneeScol chargerUnEnregistrement(JSONObject json){
+		AnneeScol uneAnneeScol = new AnneeScol(null);
+		try {
+
+			uneAnneeScol.setAnneeScol(json.getString("annee"));
+
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return uneAnneeScol;
+	}
+	
+}
---
+++ src/com/grp6/gestage/fragment/StageFragment.java
@@ -0,0 +1,39 @@
+package com.grp6.gestage.fragment;
+
+import java.util.HashMap;
+
+import com.grp6.gestage.R;
+import com.grp6.gestage.library.DatabaseHandler;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class StageFragment extends Fragment {
+
+
+	public StageFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_accueil, container,
+				false);
+
+
+		return rootView;
+	}
+
+}
